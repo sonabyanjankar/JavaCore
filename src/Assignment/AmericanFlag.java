110d7458931af08dc297abb4fe80af5beef8b098
sonabyanjankar <54607479+sonabyanjankar@users.noreply.github.com>
2021-02-24 11:56:51 -0600

    American flag class

--- src/Assignment/AmericanFlag.java
+++ src/Assignment/AmericanFlag.java
@@ -3,7 +3,12 @@ package Assignment;
 public class AmericanFlag {
     public static void main(String[] args) {
 
-        System.out.println("Flag");
-
-    }
+        System.out.println("American Flag");
+        for (int i =0; i <2 ; i++){
+            System.out.println("******------------");
+            }
+        for(int j=0; j<3; j++){
+            System.out.println("******************");
+        }
+        }
 }
