9abc6535e7cfa4b4f53f2a6d6c5b4e8fd904cb25
sonabyanjankar <54607479+sonabyanjankar@users.noreply.github.com>
2021-02-24 11:12:53 -0600

    changes

---
+++ src/Classwork/Welcome.java
@@ -0,0 +1,15 @@
+package Classwork;
+
+
+public class Welcome {
+
+    public static void main(String[] args) {
+        
+       String name= "Welcome to Java programming";
+       for(int i=0; i<5 ; i++) {
+           System.out.println(name);
+       }
+
+        
+    }
+}
