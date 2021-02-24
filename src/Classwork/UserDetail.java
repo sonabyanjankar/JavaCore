21d4a4565c47c1b47f387327958f7ccf7dc6bf14
sonabyanjankar <54607479+sonabyanjankar@users.noreply.github.com>
2021-02-24 11:11:21 -0600

    changes

---
+++ src/Classwork/UserDetail.java
@@ -0,0 +1,19 @@
+package Classwork;
+
+public class UserDetail {
+
+    public static void main(String[] args) {
+
+        int id = 1;
+        String name="\"Sona\"";
+        String address=" 'Irving, Texas' ";
+        String dob= "01-4-1996";
+
+        System.out.println(" USER DETAILS ");
+        System.out.println("Id : "+ id);
+        System.out.println("Name : " + name);
+        System.out.println("Address : "+ address);
+        System.out.println("DOB :"+ dob);
+
+    }
+}
