eb66cc4a144d07162cf1ac87bf8b1187a18ec08d
sonabyanjankar <54607479+sonabyanjankar@users.noreply.github.com>
2021-02-25 11:04:19 -0600

    Datatype , datatypereport added

---
+++ src/Classwork/Datatype.java
@@ -0,0 +1,11 @@
+package Classwork;
+
+public class Datatype {
+
+    public static void main(String[] args) {
+        short s = 250;
+        byte b = (byte)s; // explicit - forced
+        System.out.println(b);
+
+    }
+}
---
+++ src/Classwork/DatatypeReport.java
@@ -0,0 +1,20 @@
+package Classwork;
+
+public class DatatypeReport {
+
+    public static void main(String[] args) {
+        System.out.println("DataType Report");
+
+        System.out.println("DataType\tMinValue\tMaxValue");
+        System.out.println("Byte\t"+ "\t" +Byte.MIN_VALUE + "\t \t" +Byte.MAX_VALUE );
+
+        System.out.println("Short\t"+ "\t" +Short.MIN_VALUE + "\t \t" +Short.MAX_VALUE );
+
+        System.out.println("Integer\t"+"\t" + Integer.MIN_VALUE + "\t " + Integer.MAX_VALUE );
+
+        System.out.println("Float\t"+"\t" + Float.MIN_VALUE + "\t\t " + Float.MAX_VALUE );
+
+
+
+    }
+}
